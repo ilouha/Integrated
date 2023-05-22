94b7ea97d74c393cf4f862560c1155706160e748
ilouha <63893683+ilouha@users.noreply.github.com>
2023-05-22 15:33:05 -0700

    updates

--- ProformaCalcs.py
+++ ProformaCalcs.py
@@ -1,51 +1,50 @@
-import pandas as pd 
+import pandas as pd
 
-#Variable Declaration 
+# Variable Declaration
+units = 20  # Number of units in the project
+total_area = 20000  # Total area of the project in square feet
+total_parking_area = 3000 # Total area of the parking in square feet
+rpsf = 4  # Rent per square foot
+ppsf = 800  # Price per square foot for unit sale
+cpsf = 350  # Construction cost per square foot
+cpsf_parking = 175 # Construction cost per square foot for parking
+cap_rate = 5  # Capitalization rate
+hard_soft_coef = 80  # Percentage of construction costs attributed to hard costs
+net_coef = 70  # Percentage of average rent considered as net operating income
+land_value = 1000000  # Value of the land
 
-units = 20
-total_area = 20000
-rpsf = 4
-ppsf = 800
-cpsf = 350
-cap_rate = 5
-hard_soft_coef = 80
-net_coef = 70
-land_value  = 1000000
+# Calculations
+average_unit_size = total_area / units  # Average size of each unit in square feet
+average_rent = float(rpsf) * total_area / units  # Average monthly rent per unit
+unit_sale_value = average_unit_size * ppsf  # Sale value of each unit
 
-#calcs
+project_valuation = (average_rent * units * (net_coef / 100) * 12) / (cap_rate / 100)  # Valuation of the project based on net operating income
+construction_costs = total_area * cpsf  # Total construction costs for the project
+soft_costs = construction_costs * ((100 - hard_soft_coef) / 100)  # Soft costs for the project
+total_project_costs = land_value + construction_costs + soft_costs  # Total costs for the project
 
-average_unit_size = total_area/units
-average_rent = float(rpsf) * (total_area)/units
-unit_sale_value = average_unit_size * ppsf
-
-project_valuation = (average_rent * units * (net_coef/100)*12) / (cap_rate/100)
-construction_costs = total_area*cpsf
-soft_costs = construction_costs*((100-hard_soft_coef)/100)
-total_project_costs = land_value + construction_costs + soft_costs
-
-#____________________________________________________________
-
+# Create a dictionary to store the calculated values
 data_dict = {
-
-    'total_area': total_area,
+    'total__buildbale_area': total_area,
+    'total_parking_area': total_parking_area,
     'total_units': units,
     'average_unit_size': average_unit_size,
     'average_unit_rent': average_rent,
     'average_unit_sale_value': unit_sale_value,
     'land_value': land_value,
-    'construction_cost': total_area * cpsf,
+    'construction_cost': total_area * cpsf + total_parking_area * cpsf_parking,
     'soft_costs': soft_costs,
     'total_cost': total_project_costs,
     'sale_value': total_area * ppsf,
     'project_valuation': project_valuation,
     'gross_profit': total_area * ppsf - total_project_costs,
-    'gross_margin': (total_area * ppsf - total_project_costs) / (total_area * ppsf)
+    'gross_margin': (total_area * ppsf - total_project_costs) / (total_area * ppsf),
 }
 
-#iterate through the dictionary and create a string out the key and value
-
+# Create a list of keys to iterate through
 keys_list = [
-    'total_area',
+    'total__buildbale_area',
+    'total_parking_area',
     'total_units',
     'average_unit_size',
     'average_unit_rent',
@@ -57,14 +56,20 @@ keys_list = [
     'sale_value',
     'project_valuation',
     'gross_profit',
-    'gross_margin'
+    'gross_margin',
 ]
 
-#iterate through key_list and return the value from the data_dict
-
+# Iterate through the keys_list and retrieve corresponding values from the data_dict
 values = []
 for item in keys_list:
     values.append(item + ': ' + str(data_dict[item]))
 
-
+# Print the values
 print(values)
+
+# Create a dataframe from the data_dict
+df = pd.DataFrame(data_dict, index=[0])
+#flip df
+df = df.transpose()
+#save the df to a csv file
+df.to_csv('./test.csv', sep=',', encoding='utf-8')
